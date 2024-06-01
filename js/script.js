3eef6dc95eff6883677f63db4c2b3f729c2ccef8
Active Themez <omar.codezone@gmail.com>
2024-06-01 21:12:05 +0300

    gg

---
+++ js/script.js
@@ -0,0 +1,88 @@
+(function ($) {
+	"use strict";
+
+	// Theme color control js
+
+	// Mobile menu control js
+	$(".mobile-menu-control-bar").on("click", function () {
+		$(".mobile-menu-overlay").addClass("show");
+		$(".navbar-main").addClass("show");
+	})
+	$(".mobile-menu-overlay").on("click", function () {
+		$(".mobile-menu-overlay").removeClass("show");
+		$(".navbar-main").removeClass("show");
+	})
+
+	// Parallax scroll effect js
+	document.querySelectorAll(".move-with-cursor").forEach(a => {
+		document.addEventListener("mousemove", function (e) {
+			var t = e.clientX,
+				e = e.clientY;
+			a.style.transition = "transform 0.6s cubic-bezier(0.34, 1.56, 0.64, 1)", a.style.transform = `translate(${.01 * t}px, ${.01 * e}px) rotate(${.01 * (t + e)}deg)`
+		})
+	}),
+
+		// Email copy button js
+		new ClipboardJS('.btn-copy');
+
+	// Email copy button tooltip js
+	$(document).ready(function () {
+		$(".btn-copy").on("click", function () {
+			$(this).addClass("active");
+
+			setTimeout(() => {
+				$(this).removeClass("active");
+			}, 1000);
+		});
+	});
+
+	// Magnific popup js
+	$(".parent-container").magnificPopup({
+		delegate: ".gallery-popup",
+		type: "image",
+		gallery: {
+			enabled: true,
+		},
+	});
+
+	// Client feedback slider js
+	$(".client-feedback-slider").slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		autoplay: false,
+		dots: false,
+		infinite: true,
+		arrows: true,
+		speed: 500,
+		prevArrow: '<i class="fas left icon fa-arrow-left"></i>',
+		nextArrow: '<i class="fas right icon fa-arrow-right"></i>',
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			}
+		},]
+	});
+
+	// Article publications slider js
+	$(".article-publications-slider").slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		autoplay: false,
+		dots: false,
+		infinite: true,
+		arrows: true,
+		speed: 500,
+		prevArrow: '<i class="fas left icon fa-arrow-left"></i>',
+		nextArrow: '<i class="fas right icon fa-arrow-right"></i>',
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			}
+		},]
+	});
+
+})(jQuery);
